9725b7bd2ccd4069e3874a26c18247a5040f4dfe
Sharmisha <Sharmisha369@gmail.com>
2024-04-12 00:34:50 -0500

    no changes'

--- Joke generation - GPT-2/config.py
+++
@@ -1,9 +0,0 @@
-from transformers import GPT2Tokenizer
-
-BATCH_SIZE = 16
-EPOCHS = 4
-LEARNING_RATE = 2e-5
-MAX_LEN = 64
-TRAIN_PATH = "./jokes.csv"  #ADD PATH TO YOUR DATASET HERE
-MODEL_FOLDER = "Joke Generation" 
-Tokenizer = GPT2Tokenizer.from_pretrained('gpt2-medium')
